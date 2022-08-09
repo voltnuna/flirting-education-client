5f85da3b810223ed497dec172ea50654c88af1a9
doviisdev.git@gmail.com <doviisdev.git@gmail.com>
2022-08-09 14:59:05 +0900

    eject 후 webpack alias 설정 + scss 초기 셋팅

--- public/electron.js
+++ public/electron.js
@@ -1,91 +1,50 @@
-// Module to control the application lifecycle and the native browser window.
-const { app, BrowserWindow, protocol } = require("electron");
-const path = require("path");
-const url = require("url");
-
-// Create the native browser window.
-function createWindow() {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    // Set the path of an additional "preload" script that can be used to
-    // communicate between node-land and browser-land.
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
-  });
-
-  // In production, set the initial browser path to the local bundle generated
-  // by the Create React App build process.
-  // In development, set it to localhost to allow live/hot-reloading.
-  const appURL = app.isPackaged
-    ? url.format({
-        pathname: path.join(__dirname, "index.html"),
-        protocol: "file:",
-        slashes: true,
-      })
-    : "http://localhost:3000";
-  mainWindow.loadURL(appURL);
-
-  // Automatically open Chrome's DevTools in development mode.
-  if (!app.isPackaged) {
-    mainWindow.webContents.openDevTools();
-  }
-}
-
-// Setup a local proxy to adjust the paths of requested files when loading
-// them from the local production bundle (e.g.: local fonts, etc...).
-function setupLocalFilesNormalizerProxy() {
-  protocol.registerHttpProtocol(
-    "file",
-    (request, callback) => {
-      const url = request.url.substr(8);
-      callback({ path: path.normalize(`${__dirname}/${url}`) });
-    },
-    (error) => {
-      if (error) console.error("Failed to register protocol");
+"use strict";
+exports.__esModule = true;
+var electron_1 = require("electron");
+var isDev = require("electron-is-dev");
+var path = require("path");
+var mainWindow;
+var createWindow = function () {
+    mainWindow = new electron_1.BrowserWindow({
+        width: 1600,
+        height: 900,
+        center: true,
+        kiosk: !isDev,
+        resizable: true,
+        fullscreen: false,
+        fullscreenable: true,
+        webPreferences: {
+            // node환경처럼 사용하기
+            nodeIntegration: true,
+            // 개발자도구
+            devTools: isDev
+        }
+    });
+    // production에서는 패키지 내부 리소스에 접근.
+    // 개발 중에는 개발 도구에서 호스팅하는 주소에서 로드.
+    mainWindow.loadURL(isDev
+        ? "http://localhost:3000"
+        : "file://".concat(path.join(__dirname, "../build/index.html")));
+    if (isDev) {
+        mainWindow.webContents.openDevTools({ mode: "detach" });
     }
-  );
-}
-
-// This method will be called when Electron has finished its initialization and
-// is ready to create the browser windows.
+    mainWindow.setResizable(true);
+    // Emitted when the window is closed.
+    mainWindow.on("closed", function () { return (mainWindow = undefined); });
+    mainWindow.focus();
+};
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
-  setupLocalFilesNormalizerProxy();
-
-  app.on("activate", function() {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+electron_1.app.on("ready", createWindow);
+// Quit when all windows are closed.
+electron_1.app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") {
+        electron_1.app.quit();
     }
-  });
 });
-
-// Quit when all windows are closed, except on macOS.
-// There, it's common for applications and their menu bar to stay active until
-// the user quits  explicitly with Cmd + Q.
-app.on("window-all-closed", function() {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-// If your app has no need to navigate or only needs to navigate to known pages,
-// it is a good idea to limit navigation outright to that known scope,
-// disallowing any other kinds of navigation.
-const allowedNavigationDestinations = "https://my-electron-app.com";
-app.on("web-contents-created", (event, contents) => {
-  contents.on("will-navigate", (event, navigationUrl) => {
-    const parsedUrl = new URL(navigationUrl);
-
-    if (!allowedNavigationDestinations.includes(parsedUrl.origin)) {
-      event.preventDefault();
+electron_1.app.on("activate", function () {
+    if (mainWindow === null) {
+        createWindow();
     }
-  });
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- src/components/InputForm/index.tsx
+++ src/components/InputForm/index.tsx
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const InputForm = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div>gkgkgkkg</div>;
 };
 
 export default InputForm;
